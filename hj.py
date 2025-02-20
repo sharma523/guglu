90fa7fc99cd5cc07a6b30ca0316618d028d129ec
sharma523 <sharmaguglu11@gmail.com>
2025-02-20 09:13:22 -0800

    Add files via upload

---
+++ hj.py
@@ -0,0 +1,8 @@
+import os
+if os.path.exists("C:\\Users\\DELL\\Desktop\\New folder py\A.txt"):
+   os.remove("C:\\Users\\DELL\\Desktop\\New folder py\A.txt")
+else:
+   print("file not alable....!")
+
+print("file delete  success")                       
+    
